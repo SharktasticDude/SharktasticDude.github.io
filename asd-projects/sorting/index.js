1f6806cd332bb3174fbaae02e980e36678e7cc1d
SharktasticDude <sharktasticdude@gmail.com>
2026-04-15 16:36:14 +0000

    saving sorting exercise

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,16 +14,65 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+async function bubbleSort(array) {
+    for (var i = 0; i < array.length; i++) {
+        for (var j = array.length - 1; j >= i + 1; j--) {
+            if (array[j].value < array[j - 1].value) {
+                swap(array, j, j - 1);
+                updateCounter(bubbleCounter);
+                await sleep();
+            }
+        }
+    }
+}
 
 // TODO 3: Implement quickSort
+async function quickSort(array, left, right) {
 
+    if (right - left) {
+        var index = await partition(array, left, right);
+        if (left < (index - 1)) {
+            await quickSort(array, left, index - 1)
+        }
+        if (index < right) {
+            await quickSort(array, index, right)
+        }
+    }
+}
 
 // TODOs 4 & 5: Implement partition
+async function partition(array, left, right) {
+    pivot = array[Math.floor((right + left) / 2)].value;
 
+    while (left < right) {
+        while (array[left].value < pivot) {
+            left++
+        }
+        while (array[right].value > pivot) {
+            right--
+        }
+
+        if (left < right) {
+            swap(array, left, right)
+            updateCounter(quickCounter);
+            await sleep();
+        }
+
+
+
+    }
+    return left + 1;
+
+
+}
 
 // TODO 1: Implement swap
-
+function swap(array, i, j) {
+    var temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+    drawSwap(array, i, j);
+}
 
 ///////////////////////////////////////////////////////////////////////
 /////////////////////// YOUR WORK GOES ABOVE HERE /////////////////////
@@ -32,12 +81,12 @@ The CSS ids you will work with are:
 //////////////////////////// HELPER FUNCTIONS /////////////////////////
 
 // this function makes the program pause by SLEEP_AMOUNT milliseconds whenever it is called
-function sleep(){
+function sleep() {
     return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
 }
 
 // This function draws the swap on the screen
-function drawSwap(array, i, j){
+function drawSwap(array, i, j) {
     let element1 = array[i];
     let element2 = array[j];
 
@@ -48,6 +97,6 @@ function drawSwap(array, i, j){
 }
 
 // This function updates the specified counter
-function updateCounter(counter){
+function updateCounter(counter) {
     $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
 }
